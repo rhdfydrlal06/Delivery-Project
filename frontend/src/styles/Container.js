5fc2c314624a10338058bbaa97811c9c7660903b
김나현 <nahyun@gimnahyeon-ui-MacBookAir.local>
2021-09-30 10:41:46 +0900

    first setting

---
+++ frontend/src/styles/Container.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CenterBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 300px;
+`;
