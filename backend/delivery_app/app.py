6768d9f53e3031aaf35551fd934b0e99da21f5ee
tanficial <tanficial9574@gmail.com>
2021-10-01 11:53:20 +0900

    feat: sample geodata list api

--- backend/delivery_app/app.py
+++ backend/delivery_app/app.py
@@ -1,9 +1,17 @@
 """
 delivery_app 생성 및 실행
 """
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+import config
+
+from apis import geodata_api
+
+
+db = SQLAlchemy()
 
 def create_app():
     '''
@@ -11,8 +19,15 @@ def create_app():
     input:
     output: app
     '''
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     CORS(app, supports_credentials=True)
+
+    app.config.from_object(config)
+    db.init_app(app)
+    Migrate().init_app(app, db)
+
+    app.register_blueprint(geodata_api.bp, url_prefix='/api/geodata')
+
 
     @app.route("/")
     def landing():
