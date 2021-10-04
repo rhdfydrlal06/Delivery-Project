8c081d9fc3101a48809ac92790368b03d9302a1d
nahyunn <nhkim0711@gmail.com>
2021-10-04 21:58:59 +0900

    geoJSON 지도

--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,90 +1,38 @@
-import { MapContainer, TileLayer, Polygon, Marker, useMap, Rectangle } from "react-leaflet"
-import seoul from "./area/Seoul"
-import region from "./area/makerPosition"
-import { map } from "leaflet"
-import { useState, useMemo } from "react"
-
-const regionList = Object.values(region)
-
-const innerBoundsSeoul = [
-  [37.715133, 126.734086],
-  [37.413294, 127.269311],
-]
-
-const outerBounds = [
-  [38.9, 124.5],
-  [33.0, 132.0],
-]
-
-const redColor = { color: "red" }
-const whiteColor = { color: "white" }
-
-const SetBoundsRectangles = () => {
-  const [bounds, setBounds] = useState(outerBounds)
-  const map = useMap()
-
-  const innerHandlers = useMemo(
-    () => ({
-      click() {
-        setBounds(innerBoundsSeoul)
-        map.fitBounds(innerBoundsSeoul)
-      },
-    }),
-    [map],
-  )
-  const outerHandlers = useMemo(
-    () => ({
-      click() {
-        setBounds(outerBounds)
-        map.fitBounds(outerBounds)
-      },
-    }),
-    [map],
-  )
-
-  return (
-    <>
-      <Rectangle
-        bounds={outerBounds}
-        eventHandlers={outerHandlers}
-        pathOptions={bounds === outerBounds ? redColor : whiteColor}
-      />
-      <Rectangle
-        bounds={innerBoundsSeoul}
-        eventHandlers={innerHandlers}
-        pathOptions={bounds === innerBoundsSeoul ? redColor : whiteColor}
-      />
-    </>
-  )
-}
+import { MapContainer, GeoJSON } from "react-leaflet"
+import mapData from "../data/korea_region.json"
+import "leaflet/dist/leaflet.css"
 
 const Map = () => {
-  // 지도 css는 이 곳에 작성해주세요
   const mapStyle = {
     height: "100%",
+    backgroundColor: "white",
   }
 
-  const test = [
-    [36.38, 127.51],
-    [37.38, 127.55],
-    [36.48, 126.51],
-  ]
+  const regionStyle = {}
 
-  const markers = regionList.map(region => {
-    return <Marker position={region} />
-  })
+  const onEachCountry = (feature, layer) => {
+    const regionName = feature.properties.CTP_KOR_NM
+    layer.on({
+      click: e => {
+        console.log("click", regionName)
+      },
+      mouseover: e => {
+        e.target.setStyle({
+          fillOpacity: 0.5,
+        })
+      },
+      mouseout: e => {
+        e.target.setStyle({
+          fillOpacity: 0.2,
+        })
+      },
+    })
+  }
 
   return (
     <>
-      <MapContainer style={mapStyle} center={[36.38, 127.51]} zoom={6} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* <Polygon pathOptions={{ color: "red" }} positions={seoul} /> */}
-        {/* <Polygon pathOptions={{ color: "red" }} positions={test} /> */}
-        {markers}
-        <SetBoundsRectangles />
+      <MapContainer style={mapStyle} center={[36.38, 127.51]} zoom={7} scrollWheelZoom={true}>
+        <GeoJSON style={regionStyle} data={mapData.features} onEachFeature={onEachCountry} />
       </MapContainer>
     </>
   )
