6f92e3443d92c9c0ccc8b9f2e576ace41d398197
nahyunn <nhkim0711@gmail.com>
2021-10-04 23:03:07 +0900

    Map.js 약간 수정

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -2,37 +2,39 @@ import { MapContainer, GeoJSON } from "react-leaflet"
 import mapData from "../data/korea_region.json"
 import "leaflet/dist/leaflet.css"
 
+const mapStyle = {
+  height: "100%",
+  backgroundColor: "white",
+}
+
+// 추후 작업 예정
+const regionStyle = {}
+
 const Map = () => {
-  const mapStyle = {
-    height: "100%",
-    backgroundColor: "white",
-  }
-
-  const regionStyle = {}
-
-  const onEachCountry = (feature, layer) => {
+  const onEachRegion = (feature, layer) => {
     const regionName = feature.properties.CTP_KOR_NM
+
+    const handleRegionClick = e => {
+      console.log("click", regionName)
+    }
+
+    const setHover = e => {
+      e.target.setStyle({
+        fillOpacity: e.type == "mouseover" ? 0.5 : 0.2,
+      })
+    }
+
     layer.on({
-      click: e => {
-        console.log("click", regionName)
-      },
-      mouseover: e => {
-        e.target.setStyle({
-          fillOpacity: 0.5,
-        })
-      },
-      mouseout: e => {
-        e.target.setStyle({
-          fillOpacity: 0.2,
-        })
-      },
+      click: handleRegionClick,
+      mouseover: setHover,
+      mouseout: setHover,
     })
   }
 
   return (
     <>
       <MapContainer style={mapStyle} center={[36.38, 127.51]} zoom={7} scrollWheelZoom={true}>
-        <GeoJSON style={regionStyle} data={mapData.features} onEachFeature={onEachCountry} />
+        <GeoJSON style={regionStyle} data={mapData.features} onEachFeature={onEachRegion} />
       </MapContainer>
     </>
   )
