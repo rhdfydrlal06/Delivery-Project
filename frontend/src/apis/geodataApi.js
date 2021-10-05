f8d50d3be0bbdb0ac9590dbcc28eac2397e2d33f
tanficial <tanficial9574@gmail.com>
2021-10-05 18:14:15 +0900

    지역 data fetch 구현

---
+++ frontend/src/apis/geodataApi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const apiPath = process.env.REACT_APP_BACKEND_URL + "/api/geodata";
+
+export const geodataListRequest = async () => {
+    const response = await axios.get(`${apiPath}/list`);
+    return response.data;
+}
+
+export const storesGraphByIdRequest = async (id) => {
+    const response = await axios.get(`${apiPath}/graph/stores/${id}`);
+    return response.data
+}
+
+export const orderGraphByIdRequest = async (id) => {
+    const response = await axios.get(`${apiPath}/graph/order-per-time/${id}`);
+    return response.data
+}
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,6 +1,8 @@
+import { useEffect, useState, useRef } from "react"
 import { MapContainer, GeoJSON } from "react-leaflet"
-import mapData from "../data/korea_region.json"
+import mapJSON from "../data/korea_region.json"
 import "leaflet/dist/leaflet.css"
+import { geodataListRequest, storesGraphByIdRequest, orderGraphByIdRequest } from "../apis/geodataApi"
 
 const mapStyle = {
   height: "100%",
@@ -11,11 +13,50 @@ const mapStyle = {
 const regionStyle = {}
 
 const Map = () => {
+  const [mapData, setMapData] = useState(mapJSON)
+  const mapRef = useRef()
+
+  useEffect(() => {
+    async function fetchData() {
+      const geodata = await geodataListRequest()
+      const enrichedMapFeatures = mapData.features.map(element => {
+        const geoID = geodata.data.find(d => d.location1 === element.properties.CTP_KOR_NM).id
+        element.properties.ID = geoID
+        return element
+      });
+      setMapData(prev => {
+        return {
+          ...prev,
+          features: enrichedMapFeatures
+        }
+      })
+    }
+    if (!mapData.features[0].properties.ID) {
+      fetchData()
+    }
+  }, [mapData])
+
   const onEachRegion = (feature, layer) => {
-    const regionName = feature.properties.CTP_KOR_NM
+    const graphTest = async (id) => {
+      const result = await storesGraphByIdRequest(id)
+      alert(JSON.stringify(result))
+    }
 
     const handleRegionClick = e => {
+      const regionName = feature.properties.CTP_KOR_NM
+      const regionID = feature.properties.ID
       console.log("click", regionName)
+      console.log("click", regionID)
+
+      mapRef.current.setStyle({
+        fillColor: "#3388ff",
+      })
+
+      e.target.setStyle({
+        fillColor: "#ff0000",
+      })
+
+      graphTest(regionID)
     }
 
     const setHover = e => {
@@ -33,8 +74,8 @@ const Map = () => {
 
   return (
     <>
-      <MapContainer style={mapStyle} center={[36.38, 127.51]} zoom={7} scrollWheelZoom={true}>
-        <GeoJSON style={regionStyle} data={mapData.features} onEachFeature={onEachRegion} />
+      <MapContainer style={mapStyle} center={[36, 127.51]} zoom={7} scrollWheelZoom={false}>
+        <GeoJSON ref={mapRef} style={regionStyle} data={mapData.features} onEachFeature={onEachRegion} />
       </MapContainer>
     </>
   )
