9a08d07f8e1d3854532ed41ddd4b0ecbd417d617
nahyunn <nhkim0711@gmail.com>
2021-10-05 18:15:56 +0900

    MUI 적용

--- frontend/src/pages/DeliveryNum.js
+++ frontend/src/pages/DeliveryNum.js
@@ -1,13 +1,8 @@
 import { useHistory } from "react-router"
 import Map from "../components/Map"
-import {
-  WholeContainer,
-  MainBodyContainer,
-  ContentsBoxRight,
-  ContentsBoxLeft,
-} from "../styles/Container"
-import NavBar from "../components/NavBar"
+import { WholeContainer, MenuBox, ContentsBox, MapBox, LogoBox } from "../styles/Container"
 import { MainButton } from "../styles/Buttons"
+import Menu from "../components/Menu"
 
 const DeliveryNum = () => {
   const history = useHistory()
@@ -18,17 +13,29 @@ const DeliveryNum = () => {
 
   return (
     <WholeContainer>
-      <NavBar />
-      <MainBodyContainer>
-        <ContentsBoxLeft>
-          <h1>전국 코로나 확진자수 대비 배달 건수 비교</h1>
-          <p>~~데이터 분석 결과 간단 설명~~</p>
-          <MainButton onClick={handleSubmit}>메인 페이지로</MainButton>
-        </ContentsBoxLeft>
-        <ContentsBoxRight>
-          <Map />
-        </ContentsBoxRight>
-      </MainBodyContainer>
+      <LogoBox onClick={handleSubmit}>
+        <img src="/img/delivery_logo.png" alt="logo" height="100%" />
+      </LogoBox>
+      <MenuBox>
+        <Menu />
+      </MenuBox>
+      <ContentsBox>
+        <h1>배달 데이터 분석</h1>
+        <p>
+          여기에는 그래프이미지 분석 내용 등등이 들어갑니다!
+          <br />
+          <br />
+          내 음식은 왜이렇게 안오지..? 오늘은 뭘 먹어야 하지..? 배달로 편리한 삶을 살아가고 있지만
+          가끔은 고민과 의문점이 생기기도 하지 않나요?
+          <br />
+          <br />
+          배달이 일상이 되어버린 우리를 위한 배달 정보 타파 서비스! 딜푸파와 함께 대한민국 배달의
+          모든것을 알아보아요~
+        </p>
+      </ContentsBox>
+      <MapBox>
+        <Map />
+      </MapBox>
     </WholeContainer>
   )
 }
--- frontend/src/styles/Container.js
+++ frontend/src/styles/Container.js
@@ -1,22 +1,46 @@
 import styled from "styled-components"
 
 export const WholeContainer = styled.div`
-  height: 100vh;
-  margin: 0 10vw;
-  display: flex;
-  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 3fr 2fr;
+  column-gap: 32px;
+  grid-template-rows: 15vh auto;
+`
+
+export const LogoBox = styled.div`
+  grid-column: 1/2;
+  grid-row: 1/2;
+  padding: 32px 24px 24px 32px;
+  cursor: pointer;
+`
+
+export const MapBox = styled.div`
+  grid-column: 3/4;
+  grid-row: 1/3;
+  margin: 32px 32px 32px 0px;
+  border: 1px solid black;
+`
+
+export const MenuBox = styled.div`
+  grid-column: 1/2;
+  grid-row: 2/3;
+`
+
+export const ContentsBox = styled.div`
+  grid-column: 2/3;
+  grid-row: 2/3;
 `
 
 export const MainBodyContainer = styled.div`
   display: flex;
+  flex-direction: column;
   text-align: left;
-  flex-grow: 1;
-  margin-bottom: 10vh;
 `
 
 export const ContentsBoxRight = styled.div`
-  width: 50%;
-  margin-left: 4%;
+  width: 33%;
 `
 
 export const ContentsBoxLeft = styled.div`
