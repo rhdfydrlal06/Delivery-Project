4c58b1269a727d62fcfb445dea8ff29db1fc246e
tanficial <tanficial9574@gmail.com>
2021-10-11 21:48:05 +0900

    migrate 스크립트 환경변수 수정

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -1,8 +1,9 @@
 export FLASK_APP=delivery_app.app
 export FLASK_ENV=production
-export DB_USER=[]
+export DB_USER=root
+export DB_HOST=localhost
 export DB_PASSWORD=[]
-export DB_NAME=[]
+export DB_NAME=delivery
 # python -c "import os; print(os.urandom(16))"
 export SECRET_KEY=[]
 
