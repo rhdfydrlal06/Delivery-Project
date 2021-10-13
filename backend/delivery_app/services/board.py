2e63c01f01c9e83844526891f8bba949ee2aaf1e
[2기-1반]김희재 <gmlwo0109@gmail.com>
2021-10-13 10:13:13 +0900

    board api 추가 수정

--- backend/delivery_app/services/board.py
+++ backend/delivery_app/services/board.py
@@ -7,7 +7,11 @@ def get_post(post_id):
     input: post_id(int)
     output: Posts 객체 하나
     """
-    return Posts.query.filter_by(id=post_id).one_or_none()
+    _post = Posts.query.filter_by(id=post_id).one_or_none()
+    _post.hit += 1
+    db.session.add(_post)
+    db.session.commit()
+    return _post
 
 
 def get_posts(location1, location2, category):
@@ -26,7 +30,7 @@ def get_posts(location1, location2, category):
     return result.all()
 
 
-def add_post(location1, location2, category, food, post, image, user):
+def add_post(location1, location2, food, post, image, user):
     """
     입력받은 게시글 저장 후 location1, location2, category에 해당하는 게시글 반환
     """
@@ -34,7 +38,6 @@ def add_post(location1, location2, category, food, post, image, user):
         new_post = Posts(
             location1=location1,
             location2=location2,
-            category=category,
             food=food,
             post=post,
             image=image,
@@ -47,16 +50,39 @@ def add_post(location1, location2, category, food, post, image, user):
         db.session.rollback()
         raise
 
+def edit_post(post_id, location1, location2, food, post, image):
+    """
+    user를 제외한 수정된 데이터를 가져와 DB 값 수정(수정 범위 논의)
+    """
+    try:
+        _post = Posts.query.filter_by(id=post_id).one_or_none()
+        if _post is None:
+            return None
+
+        _post = Posts.query.filter_by(id=post_id).one_or_none()
+        _post.location1 = location1
+        _post.location2 = location2
+        _post.food = food
+        _post.post = post
+        _post.image = image
+        db.session.add(_post)
+        db.session.commit()
+        return _post.id
+
+    except Exception:
+        db.session.rollback()
+        raise
+
 
 def delete_post(post_id):
     try:
-        post = Posts.query.filter_by(id=post_id).one_or_none()
-        if post is None:
+        _post = Posts.query.filter_by(id=post_id).one_or_none()
+        if _post is None:
             return None
 
-        db.session.delete(post)
+        db.session.delete(_post)
         db.session.commit()
-        return post.id
+        return _post.id
     except Exception:
         db.session.rollback()
         raise
