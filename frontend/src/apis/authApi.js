69f726edc11d6663ac30cfdf57aa0f22e036a64b
tanficial <tanficial9574@gmail.com>
2021-10-15 01:56:58 +0900

    프론트엔드 토큰 refresh 요청 추가, 토큰 저장 세션스토리지 -> 로컬스토리지 변경

--- frontend/src/apis/authApi.js
+++ frontend/src/apis/authApi.js
@@ -3,7 +3,26 @@ import jwt from "jwt-decode"
 
 const apiPath = process.env.REACT_APP_BACKEND_URL + "/api/auth"
 
-const isExpired = (token) => {
+if (window.localStorage.getItem("token")) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${window.localStorage.getItem("token")}`
+}
+
+axios.interceptors.request.use(
+    async (config) => {
+        const token = window.localStorage.getItem("token")
+        const splitedUrl = config.url.split('/')
+        const endpoint = splitedUrl[splitedUrl.length - 1]
+        if (endpoint !== 'refresh' && token && isTokenExpired(token)) {
+            const response = await refreshTokenRequest(token)
+            const new_token = response.data.access_token
+            config.headers.Authorization = `Bearer ${new_token}`
+            return config
+        }
+        return config
+    }
+)
+
+export const isTokenExpired = (token) => {
     const decoded = jwt(token)
     return Date.now() >= decoded.exp * 1000
 }
@@ -12,11 +31,11 @@ export const signinRequest = async (email, password) => {
     try {
         const response = await axios.post(`${apiPath}/signin`, { email, password })
         const token = response.data.access_token
-        window.sessionStorage.setItem("token", token)
+        window.localStorage.setItem("token", token)
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
         return response
     } catch (error) {
-        return error
+        throw error
     }
 }
 
@@ -26,26 +45,38 @@ export const signupRequest = async (email, password, name) => {
 }
 
 export const signoutRequest = async () => {
-    const token = window.sessionStorage.getItem("token")
+    const token = window.localStorage.getItem("token")
     if (!token) return
     const id = jwt(token).sub
     try {
         const response = await axios.post(`${apiPath}/signout`, { id })
-        window.sessionStorage.removeItem("token")
+        window.localStorage.removeItem("token")
         delete axios.defaults.headers.common['Authorization']
         return response
     } catch (error) {
-        return error
+        throw error
+    }
+}
+
+export const refreshTokenRequest = async (token) => {
+    try {
+        const decoded = jwt(token)
+        const id = decoded.sub
+        const response = await axios.post(`${apiPath}/refresh`, { id })
+        const new_token = response.data.access_token
+        window.localStorage.setItem("token", new_token)
+        axios.defaults.headers.common['Authorization'] = `Bearer ${new_token}`
+        return response
+    } catch (error) {
+        window.localStorage.removeItem("token")
+        delete axios.defaults.headers.common['Authorization']
+        throw error
     }
 }
 
 export const getCurrentUserInfo = () => {
-    const token = window.sessionStorage.getItem("token")
+    const token = window.localStorage.getItem("token")
     if (!token) return
-
-    if (isExpired(token)) {
-        console.log("please refresh!")
-    }
 
     const user = jwt(token)
     const result = {
